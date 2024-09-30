83c1bdd38fdc26d1d4a575cefc42282472d46b9d
yohanii <yohanii@github.com>
2024-09-30 21:49:34 +0900

    feat: add Seller skeleton

---
+++ src/main/java/lotto/Seller.java
@@ -0,0 +1,32 @@
+package lotto;
+
+import java.util.List;
+
+public class Seller {
+
+    private final int lottoPrice;
+
+    public Seller(int lottoPrice) {
+        this.lottoPrice = lottoPrice;
+    }
+
+    public int calculateLottoCount(int money) {
+        return 0;
+    }
+
+    public Lotto getLotto() {
+        return null;
+    }
+
+    public List<Lotto> getLottos(int count) {
+        return null;
+    }
+
+    public List<Integer> getResult(List<Lotto> lottos) {
+        return null;
+    }
+
+    public double calculateRate(List<Integer> ranks) {
+        return 0D;
+    }
+}
