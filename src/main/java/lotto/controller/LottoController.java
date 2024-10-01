819e234b2f93309e39d7143cc222519d10ced773
msung99 <msung6924@naver.com>
2024-10-01 16:39:43 +0900

    feat: 로또 도메인 생성 및 일급 컬렉션 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,20 @@
 package lotto.controller;
 
+import lotto.domain.Budget;
+
 public class LottoController {
+    private Budget budget;
+
+    public void runMachine() {
+        buyLotto();
+    }
+
+    private void buyLotto() {
+        try {
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return buyLotto();
+        }
+    }
 }
---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+import lotto.Lotto;
+
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    private Lottos(final List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public static Lottos from(List<Lotto> lottos) {
+        return new Lottos(lottos);
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+}
